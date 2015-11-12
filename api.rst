123d261f3db62def3bd68887f19e4adb754baec5
Knut Johannessen <knut.johannessen@bouvet.no>
2015-11-12 13:27:56 +0100

    IS-2210: copied the sesamapi aggregator and management studio into the lake repo

--- api.rst
+++ api.rst
@@ -8,4 +8,13 @@ Introduction
 Sesam provides a RESTful API for controlling the service and also working with the data in the datahub.
 
 
+Services
+========
+
+.. contents::
+   :local:
+   :depth: 1
+
+.. cornice-autodoc::
+   :modules: lake.node.webapp.api.root
 
--- conf.py
+++ conf.py
@@ -17,11 +17,17 @@ import sys
 import os
 import shlex
 import guzzle_sphinx_theme
+import cornice
+
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+# documentation root, use os.path.abspath to make it absolute.
+sys.path.insert(0, os.path.abspath(cornice.__file__))
+
+import pprint
+print ("sys.path:%s" % pprint.pformat(sys.path))
+
 
 # -- General configuration ------------------------------------------------
 
@@ -31,7 +37,7 @@ import guzzle_sphinx_theme
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ['cornice.ext.sphinxext']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
