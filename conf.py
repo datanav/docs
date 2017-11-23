a030c238cfb2f5f1b59e9364cc893ddc04ca676a
Knut Johannessen <knut.johannessen@bouvet.no>
2017-11-23 15:23:49 +0100

    IS-4385: got rid of the '0.4.dev'-part of the html title in the docs

--- conf.py
+++ conf.py
@@ -144,7 +144,7 @@ html_theme_options = {
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = "Sesam documentation"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
@@ -161,7 +161,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
