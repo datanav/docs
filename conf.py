e503eb1c9b5c286b337202ba1fbd1c1b11ab94f7
Baard H. Rehn Johansen <baard.johansen@bouvet.no>
2021-03-16 21:55:57 +0100

    Fix copyright and broken link in html footer

--- conf.py
+++ conf.py
@@ -52,8 +52,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Sesam'
-copyright = '2018, Sesam'
-author = 'Graham Moore'
+copyright = '2021 Sesam.io AS'
+author = 'The Sesam Team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
