b8a65e12c2ac33990d534dd5014ed833206b51a6
Baard H. Rehn Johansen <baard.johansen@bouvet.no>
2022-03-10 21:01:50 +0100

    Try out pydata theme with top level menu

--- conf.py
+++ conf.py
@@ -112,16 +112,18 @@ todo_include_todos = False
 # html_theme = 'alabaster'
 # html_theme = "sphinx_rtd_theme"
 
-html_theme = 'furo'
+#html_theme = 'furo'
+html_theme = 'pydata_sphinx_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-        "announcement": "This is a preview of the upcoming Sesam Documentation. For the official documentation go to <a href=\"https://docs.sesam.io\">docs.sesam.io</a>",
-    "sidebar_hide_name": True
+    "footer_items": "copyright.html",
+    #     "announcement": "This is a preview of the upcoming Sesam Documentation. For the official documentation go to <a href=\"https://docs.sesam.io\">docs.sesam.io</a>",
+    # "sidebar_hide_name": True
 }
-
+# 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
 #html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
