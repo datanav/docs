3ae1a969d75f3e53c0039c15a21b7ce459239166
parhambarazesh <parham.barazesh@bouvet.no>
2025-04-28 16:42:47 +0200

    IS-18272: Remove copyright notice and update footer items in conf.py

--- conf.py
+++ conf.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# Copyright (C) Bouvet ASA - All Rights Reserved.
-# Unauthorized copying of this file, via any medium is strictly prohibited.
 import json
 import os
 import sys
@@ -70,7 +67,6 @@ master_doc = 'hub/index'
 
 # General information about the project.
 project = 'Sesam'
-copyright = '2023 Sesam.io AS'
 author = 'The Sesam Team'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -141,7 +137,7 @@ html_theme = 'pydata_sphinx_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    "footer_items": ["left-sidebar-bottom.html", "copyright.html", "iso.html" ],
+    "footer_items": ["left-sidebar-bottom.html", "iso.html" ],
     "navbar_end": "right-menu.html",
     'navigation_depth': 5,
     "navbar_end": "right-menu.html",
@@ -219,7 +215,7 @@ html_sidebars = {
 html_show_sphinx = False
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-#html_show_copyright = True
+html_show_copyright = False
 
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
