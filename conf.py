44f5b576465ed78a0acd9fa46396b33e2d70a8bc
parhambarazesh <parham.barazesh@bouvet.no>
2025-04-29 10:29:34 +0200

    Revert copyright information and footer items in conf.py

--- conf.py
+++ conf.py
@@ -67,6 +67,7 @@ master_doc = 'hub/index'
 
 # General information about the project.
 project = 'Sesam'
+copyright = '2023 Sesam.io AS'
 author = 'The Sesam Team'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -137,7 +138,7 @@ html_theme = 'pydata_sphinx_theme'
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    "footer_items": ["left-sidebar-bottom.html", "iso.html" ],
+    "footer_items": ["left-sidebar-bottom.html", "copyright.html", "iso.html" ],
     "navbar_end": "right-menu.html",
     'navigation_depth': 5,
     "navbar_end": "right-menu.html",
@@ -215,7 +216,7 @@ html_sidebars = {
 html_show_sphinx = False
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-html_show_copyright = False
+# html_show_copyright = True
 
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
